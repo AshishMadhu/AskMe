9dd30c2982783abe42030a0c3bad2e6aa115d750
AshishMadhu <ashishmadhu26@gmail.com>
2020-11-13 11:11:16 +0530

    f

--- backend/AskMe/settings.py
+++ backend/AskMe/settings.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
-    
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
